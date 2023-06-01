a4f70cfdb5185d7adfd3c81540297fa3564a9f3a
souravbiswassanto <saurov@appscode.com>
2023-06-01 18:30:20 +0600

    Custom Resource

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/souravbiswassanto/crd/pkg/apis/makecrd.com/v1alpha1"
+	client "github.com/souravbiswassanto/crd/pkg/client/clientset/versioned"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 	_ "k8s.io/code-generator"
+	"path/filepath"
 )
 
 func main() {
-	k := v1alpha1.Crd{}
-	fmt.Println(k)
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "optional")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "optional")
+	}
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		fmt.Printf("error %s\n", err.Error())
+	}
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("error while building kubernetes clientset %s\n", err.Error())
+	}
+	myClient, err := client.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("error while building custom klientset %s\n", err.Error())
+	}
+
 }
