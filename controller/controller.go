0efc885a02cd2e6a51bd8142943a745b42552802
souravbiswassanto <saurov@appscode.com>
2023-06-07 16:02:51 +0600

    added Controller

--- controller/controller.go
+++ controller/controller.go
@@ -3,23 +3,25 @@ package controller
 import (
 	"context"
 	"fmt"
+	makecrd_com "github.com/souravbiswassanto/crd/pkg/apis/makecrd.com"
 	controllerv1 "github.com/souravbiswassanto/crd/pkg/apis/makecrd.com/v1alpha1"
 	clientset "github.com/souravbiswassanto/crd/pkg/client/clientset/versioned"
 	informer "github.com/souravbiswassanto/crd/pkg/client/informers/externalversions/makecrd.com/v1alpha1"
 	lister "github.com/souravbiswassanto/crd/pkg/client/listers/makecrd.com/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	appsinformers "k8s.io/client-go/informers/apps/v1"
-	appslisters "k8s.io/client-go/listers/apps/v1"
-
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
+	appsinformers "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
+	appslisters "k8s.io/client-go/listers/apps/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -60,6 +62,7 @@ func NewController(
 			ctrl.enqueueCrds(newObj)
 		},
 		DeleteFunc: func(obj interface{}) {
+			fmt.Println("Delete function was called")
 			ctrl.enqueueCrds(obj)
 		},
 	})
@@ -141,14 +144,14 @@ func (c *Controller) ProcessNextItem() bool {
 func (c *Controller) syncHandler(key string) error {
 	namespace, name, err := cache.SplitMetaNamespaceKey(key)
 	if err != nil {
-		utilruntime.HandleError(fmt.Errorf("invalid resource key: %s", key))
+		utilruntime.HandleError(fmt.Errorf("invalid resource key: %s\n", key))
 		return nil
 	}
 	// Get the Crd resource with this namespace/name
 	crd, err := c.crdLister.Crds(namespace).Get(name)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			utilruntime.HandleError(fmt.Errorf("foo '%s' in workqueue no logner exists", key))
+			utilruntime.HandleError(fmt.Errorf("foo '%s' in workqueue no logner exists\n", key))
 			return nil
 		}
 		return err
@@ -156,15 +159,20 @@ func (c *Controller) syncHandler(key string) error {
 	if err := c.DeploymentHandler(crd); err != nil {
 		utilruntime.HandleError(fmt.Errorf("error while handling deployment: %s", err.Error()))
 	}
+
+	if err := c.ServiceHandler(crd); err != nil {
+		utilruntime.HandleError(fmt.Errorf("error while handling service\n"))
+	}
+
 	return nil
 }
 
 func (c *Controller) DeploymentHandler(crd *controllerv1.Crd) error {
 	deploymentName := crd.Spec.Name
 	if deploymentName == "" {
-		utilruntime.HandleError(fmt.Errorf(" deployment name must be specified"))
-		return nil
+		deploymentName = strings.Join(buildSlice(crd.Name, makecrd_com.Deployment), "-")
 	}
+	fmt.Println(deploymentName, " This is deployment name")
 	namespace := crd.Namespace
 	//name := crd.Name
 	deployment, err := c.deploymentsLister.Deployments(namespace).Get(deploymentName)
@@ -196,29 +204,72 @@ func (c *Controller) DeploymentHandler(crd *controllerv1.Crd) error {
 	return nil
 }
 
+func (c *Controller) ServiceHandler(crd *controllerv1.Crd) error {
+	serviceName := crd.Spec.Name
+	if serviceName == "" {
+		serviceName = strings.Join(buildSlice(crd.Name, makecrd_com.Service), makecrd_com.Dash)
+	}
+	service, err := c.kubeclientset.CoreV1().Services(crd.Namespace).Get(context.TODO(), serviceName, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		service, err = c.kubeclientset.CoreV1().Services(crd.Namespace).Create(context.TODO(), c.newService(crd), metav1.CreateOptions{})
+
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		log.Printf("Service %s created\n", service.Name)
+	}
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	_, err = c.kubeclientset.CoreV1().Services(crd.Namespace).Update(context.TODO(), c.newService(crd), metav1.UpdateOptions{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func buildSlice(s ...string) []string {
+	var t []string
+	for _, v := range s {
+		t = append(t, v)
+	}
+	return t
+}
+
 func (c *Controller) newDeployment(crd *controllerv1.Crd) *appsv1.Deployment {
+	deploymentName := crd.Spec.Name
+	if deploymentName == "" {
+		deploymentName = strings.Join(buildSlice(crd.Name, makecrd_com.Deployment), "-")
+	}
 	return &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      crd.Spec.Name,
+			Name:      deploymentName,
 			Namespace: crd.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(crd, controllerv1.SchemeGroupVersion.WithKind(makecrd_com.ResourceDefinition)),
+			},
 		},
 		Spec: appsv1.DeploymentSpec{
 			Replicas: crd.Spec.Replicas,
 			Selector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{
-					"app": "my-app",
+					makecrd_com.AppLabel:  makecrd_com.AppValue,
+					makecrd_com.NameLabel: crd.Name,
 				},
 			},
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
-						"app": "my-app",
+						makecrd_com.AppLabel:  makecrd_com.AppValue,
+						makecrd_com.NameLabel: crd.Name,
 					},
 				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
-							Name:  "my-app",
+							Name:  makecrd_com.AppValue,
 							Image: crd.Spec.Container.Image,
 							Ports: []corev1.ContainerPort{
 								{
@@ -229,6 +280,35 @@ func (c *Controller) newDeployment(crd *controllerv1.Crd) *appsv1.Deployment {
 							},
 						},
 					},
+				},
+			},
+		},
+	}
+
+}
+
+func (c *Controller) newService(crd *controllerv1.Crd) *corev1.Service {
+	serviceName := crd.Spec.Name
+	if serviceName == "" {
+		serviceName = strings.Join(buildSlice(crd.Name, makecrd_com.Service), makecrd_com.Dash)
+	}
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: serviceName,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(crd, controllerv1.SchemeGroupVersion.WithKind(makecrd_com.ResourceDefinition)),
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeNodePort,
+			Selector: map[string]string{
+				makecrd_com.AppLabel:  makecrd_com.AppValue,
+				makecrd_com.NameLabel: crd.Name,
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Port:       makecrd_com.ServicePort,
+					TargetPort: intstr.FromInt(int(crd.Spec.Container.Port)),
 				},
 			},
 		},
---
+++ pkg/apis/makecrd.com/constant.go
@@ -0,0 +1,12 @@
+package makecrd_com
+
+const (
+	ResourceDefinition = "Crd"
+	Deployment         = "dep"
+	Service            = "svc"
+	Dash               = "-"
+	AppLabel           = "app"
+	AppValue           = "my-app"
+	ServicePort        = 3200
+	NameLabel          = "name"
+)
